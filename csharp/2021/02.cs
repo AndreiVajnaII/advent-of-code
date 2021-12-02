f9b1e06e44183b01884e7b94ca99edf3a630eda7
Andrei Vajna II <AndreiVajnaII@gmail.com>
2021-12-02 12:39:28 +0200

    2021 - day 2

---
+++ csharp/2021/02.cs
@@ -0,0 +1,82 @@
+using System.Reflection;
+
+public class Solver202102 : ISolver
+{
+    public dynamic Solve(string[] lines)
+    {
+        DepthCoords depthCoords = new();
+        AimCoords aimCoords = new();
+        foreach (var command in lines.Select(ParseCommand))
+        {
+            command.Invoke(depthCoords);
+            command.Invoke(aimCoords);
+        }
+        return (depthCoords.Multiply(), aimCoords.Multiply());
+    }
+
+    private (string, int) SplitCommand(string command)
+    {
+        var terms = command.Split(" ");
+        return (terms[0], int.Parse(terms[1]));
+    }
+
+    private Action<Coords> ParseCommand(string command)
+    {
+        var terms = command.Split(" ");
+        var amount = int.Parse(terms[1]);
+        var method = typeof(Coords).GetMethod(terms[0], BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+        return coords => method?.Invoke(coords, new object[] {amount});
+    }
+}
+
+abstract class Coords
+{
+    protected int Depth = 0;
+    protected int Position = 0;
+    public abstract void Forward(int amount);
+    public abstract void Down(int amount);
+    public abstract void Up(int amount);
+    public int Multiply() {
+        return Depth * Position;
+    }
+}
+
+class DepthCoords : Coords
+{
+
+    public override void Forward(int amount)
+    {
+        Position += amount;
+    }
+
+    public override void Down(int amount)
+    {
+        Depth += amount;
+    }
+
+    public override void Up(int amount)
+    {
+        Depth -= amount;
+    }
+}
+
+class AimCoords : Coords
+{
+    private int Aim = 0;
+
+    public override void Forward(int amount)
+    {
+        Position += amount;
+        Depth += Aim * amount;
+    }
+
+    public override void Down(int amount)
+    {
+        Aim += amount;
+    }
+
+    public override void Up(int amount)
+    {
+        Aim -= amount;
+    }
+}
--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,3 +1,3 @@
 ﻿HttpClient httpClient = new();
 httpClient.DefaultRequestHeaders.Add("cookie", args[0]);
-await new Runner(new InputHandlerFactory(httpClient)).RunAsync("2021", "1");
+await new Runner(new InputHandlerFactory(httpClient)).RunAsync("2021", "2");
