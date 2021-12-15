ce71d4fe77d6f363addf3105275c1f856c490ed4
Andrei Vajna II <AndreiVajnaII@gmail.com>
2021-12-15 20:35:29 +0200

    Added duration measurement

--- csharp/Runner.cs
+++ csharp/Runner.cs
@@ -1,25 +1,48 @@
 using System.Reflection;
 
-public class Runner {
+public class Runner
+{
     private InputHandlerFactory inputHandler;
 
-    public Runner(InputHandlerFactory inputHandler) {
+    public Runner(InputHandlerFactory inputHandler)
+    {
         this.inputHandler = inputHandler;
     }
-    public async Task RunAsync(string year, string day, string? examplePath) {
+    public async Task RunAsync(string year, string day, string? examplePath)
+    {
         Console.WriteLine(
-            GetSolver(year, day)?
+            GetSolver(year, day)
                 .Solve(
                     await inputHandler.For(year, day, examplePath).GetAsync()
                 ));
     }
 
-    private static ISolver? GetSolver(string year, string day) {
-        return (ISolver?)Assembly.GetExecutingAssembly().CreateInstance("Solver" + year + Helpers.PadDay(day));
+    private static ISolver GetSolver(string year, string day)
+    {
+        return new SolverWithMeasureMent((ISolver?)Assembly.GetExecutingAssembly()
+            .CreateInstance("Solver" + year + Helpers.PadDay(day))!);
     }
 
 }
 
-public interface ISolver {
+public interface ISolver
+{
     dynamic Solve(string[] lines);
+}
+
+class SolverWithMeasureMent : ISolver
+{
+    private ISolver solver;
+    public SolverWithMeasureMent(ISolver solver)
+    {
+        this.solver = solver;
+    }
+    public dynamic Solve(string[] lines)
+    {
+        var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+        var result = solver.Solve(lines);
+        stopwatch.Stop();
+        Console.WriteLine($"Took {stopwatch.ElapsedMilliseconds:N} ms.");
+        return result;
+    }
 }
